cf7fd3d32868abb39a9d9e281de6eea14e1b369e
Vorvule <7105690@gmail.com>
2021-06-24 13:41:13 +0300

    node_modules moved to .gitignore

--- cypress/integration/home.page.spec.js
+++ cypress/integration/home.page.spec.js
@@ -1,6 +1,6 @@
 /// <reference types='cypress' />
 
-describe('dabravesce.by title page visibles testing', () => {
+describe('dabravesce.by title page main elements testing', () => {
 
     it('should check home page up', () => {
 
@@ -8,13 +8,16 @@ describe('dabravesce.by title page visibles testing', () => {
         cy.viewport(1600, 900)
 
         cy.title().should('eq', 'Дабравесце – Мф. 1')
+
         cy.get('#MenuView p').should('have.text', 'ДАБРАВЕСЦЕ')
         cy.get('#WrapperLeft p').should('have.text', 'ЗМЕСТ')
 
         cy.get('#MenuIcon').should('contain.text', 'Меню')
         cy.get('#ShareIcon').should('contain.text', 'Шырыць')
+
         cy.get('[onclick="site.page.next(false)"]').should('contain.text', 'Назад')
         cy.get('[onclick="site.page.next(true)"]').should('contain.text', 'Далей')
+
         cy.get('#SearchIcon').should('contain.text', 'Пошук')
 
     })
